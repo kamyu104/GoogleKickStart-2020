2cd109fc06f8ef7045d83e4e14fe182f854deed8
Allen Liu <kamyu104@gmail.com>
2020-10-20 03:13:05 +0800

    update

--- Round G/kick_start.py
+++ Round G/kick_start.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2020 kamyu. All rights reserved.
 #
 # Google Kick Start 2020 Round G - Problem A. Kick Start
-# https://codingcompetitions.withgoogle.com/kickstart/round/00000000001a0069/0000000000414bfb#problem
+# https://codingcompetitions.withgoogle.com/kickstart/round/00000000001a0069/0000000000414bfb
 #
 # Time:  O(N)
 # Space: O(1)
