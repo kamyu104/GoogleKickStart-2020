66067d15edf4b2cb7a41f756111f1c853adb1cf8
Allen Liu <kamyu104@gmail.com>
2020-10-20 03:12:52 +0800

    update

---
+++ Round G/kick_start.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2020 kamyu. All rights reserved.
+#
+# Google Kick Start 2020 Round G - Problem A. Kick Start
+# https://codingcompetitions.withgoogle.com/kickstart/round/00000000001a0069/0000000000414bfb#problem
+#
+# Time:  O(N)
+# Space: O(1)
+#
+
+def kick_start():
+    S = raw_input().strip()
+
+    result = count = 0
+    for i in xrange(len(S)-len(START)+1):
+        if S[i:i+len(KICK)] == KICK:
+            count += 1
+        elif S[i:i+len(START)] == START:
+            result += count
+    return result
+
+KICK = "KICK"
+START = "START"
+for case in xrange(input()):
+    print 'Case #%d: %s' % (case+1, kick_start())
