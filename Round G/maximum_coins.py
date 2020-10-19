dd2fea347e31b1ced9bf1e3f28ac08cc43d29caf
Allen Liu <kamyu104@gmail.com>
2020-10-20 03:06:41 +0800

    update

--- Round G/maximum_coins.py
+++ Round G/maximum_coins.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2020 kamyu. All rights reserved.
 #
 # Google Kick Start 2020 Round G - Problem B. Maximum Coins
-# https://codingcompetitions.withgoogle.com/kickstart/round/00000000001a0069/0000000000414a23#analysis
+# https://codingcompetitions.withgoogle.com/kickstart/round/00000000001a0069/0000000000414a23
 #
 # Time:  O(N^2)
 # Space: O(1)
