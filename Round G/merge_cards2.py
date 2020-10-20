1334658cb85e6e3e7f0ca1caee888210b4a9c793
Allen Liu <kamyu104@gmail.com>
2020-10-20 23:13:21 +0800

    update

--- Round G/merge_cards2.py
+++ Round G/merge_cards2.py
@@ -14,19 +14,19 @@
 def merge_cards():
     N = input()
     A = map(int, raw_input().strip().split())
-    return sum(a*dp[N][min(i, N-1-i)] for i, a in enumerate(A))
+    return sum(a*dp[N-1][min(i, (N-1)-i)] for i, a in enumerate(A))
 
 MAX_N = 5000
-dp = [[0.0]*((MAX_N+1)//2) for _ in xrange(MAX_N+1)]  # dp[i][j]: expected count of (j+1)th cards with total i cards 
-for i in xrange(2, MAX_N+1):  # precompute
-    for j in xrange((i+1)//2):  # dp[i] is symmetric since dp[i][j] = dp[i][i-1-j], we can only precompute left part of dp[i] to save more time
+dp = [[0.0]*((MAX_N+1)//2) for _ in xrange(MAX_N)]  # dp[i][j]: expected count of (j+1)th cards with total i+1 cards 
+for i in xrange(1, MAX_N):  # precompute
+    for j in xrange(((i+1)+1)//2):  # dp[i] is symmetric since dp[i][j] = dp[i][i-j], we can only precompute left part of dp[i] to save more time
         if j > 0:
-            dp[i][j] += ((j-1)-1+1)*dp[i-1][min(j-1, (i-2)-(j-1))]   # merge any of (0,1)~(j-2,j-1), count j with EV(i-1, j-1)
-            dp[i][j] += 1.0 + dp[i-1][min(j-1, (i-2)-(j-1))]         # merge (j-1,j), count j with 1 + EV(i-1, j-1)
-        if j < i-1:
-            dp[i][j] += 1.0 + dp[i-1][min(j, (i-2)-j)]               # merge (j,j+1), count j with 1 + EV(i-1, j)
-            dp[i][j] += ((i-1)-(j+2)+1)*dp[i-1][min(j, (i-2)-j)]     # merge any of (j+1,j+2)~(i-2,i-1), count j with EV(i-1, j)
-        dp[i][j] /= (i-1)  # (i-1) choices in i cards
+            dp[i][j] += ((j-1)-1+1)*dp[i-1][min(j-1, (i-1)-(j-1))]  # merge any of (0,1)~(j-2,j-1), count j with EV(i-1, j-1)
+            dp[i][j] += 1.0 + dp[i-1][min(j-1, (i-1)-(j-1))]        # merge (j-1,j), count j with 1 + EV(i-1, j-1)
+        if j < i:
+            dp[i][j] += 1.0 + dp[i-1][min(j, (i-1)-j)]              # merge (j,j+1), count j with 1 + EV(i-1, j)
+            dp[i][j] += (i-(j+2)+1)*dp[i-1][min(j, (i-1)-j)]        # merge any of (j+1,j+2)~(i-1,i), count j with EV(i-1, j)
+        dp[i][j] /= i  # i choices in i+1 cards
 
 for case in xrange(input()):
     print 'Case #%d: %s' % (case+1, merge_cards())
