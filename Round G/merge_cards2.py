ca1a42976c7f0ebbcfcd1eb2e239f9e3cb9d4b80
kamyu <kamyu104@users.noreply.github.com>
2020-10-20 10:47:58 +0800

    Create merge_cards2.py

---
+++ Round G/merge_cards2.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2020 kamyu. All rights reserved.
+#
+# Google Kick Start 2020 Round G - Problem D. Merge Cards
+# https://codingcompetitions.withgoogle.com/kickstart/round/00000000001a0069/0000000000415054
+#
+# Time:  O(N), neglating precomputing time  O(MAX_N^2)
+# Space: O(1), neglating precomputing space O(MAX_N^2)
+#
+# this solution is optimized from merge_cards.py
+#
+
+def merge_cards():
+    N = input()
+    A = map(int, raw_input().strip().split())
+    return sum(a*dp[N][min(i, N-1-i)] for i, a in enumerate(A))
+
+MAX_N = 5000
+dp = [[0.0]*((MAX_N+1)//2) for _ in xrange(MAX_N+1)]  # dp[i][j]: expected count of (j+1)-th cards with total i cards 
+for i in xrange(2, MAX_N+1):  # precompute
+    for j in xrange((i+1)//2):  # dp is symmetric since dp[i][j] = dp[i][i-1-j], we can only precompute left part to save more time
+        if j > 0:
+            dp[i][j] += ((j-1)-1+1)*dp[i-1][min(j-1, (i-2)-(j-1))]   # merge any of (0,1)~(j-2,j-1), count j with ev(i-1, j-1)
+            dp[i][j] += 1.0 + dp[i-1][min(j-1, (i-2)-(j-1))]         # merge (j-1,j), count j with 1 + ev(i-1, j-1)
+        if j < i-1:
+            dp[i][j] += 1.0 + dp[i-1][min(j, (i-2)-j)]               # merge (j,j+1), count j with 1 + ev(i-1, j)
+            dp[i][j] += ((i-1)-(j+2)+1)*dp[i-1][min(j, (i-2)-j)]     # merge any of (j+1,j+2)~(i-2,i-1), count j with ev(i-1, j)
+        dp[i][j] /= (i-1)  # (i-1) choices in i cards
+
+for case in xrange(input()):
+    print 'Case #%d: %s' % (case+1, merge_cards())
