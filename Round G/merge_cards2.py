c7c41dd47ae17a98747989ea70ba28a3793e7746
Allen Liu <kamyu104@gmail.com>
2020-10-20 22:13:31 +0800

    update

--- Round G/merge_cards2.py
+++ Round G/merge_cards2.py
@@ -19,11 +19,11 @@ dp = [[0.0]*((MAX_N+1)//2) for _ in xrange(MAX_N+1)]  # dp[i][j]: expected count
 for i in xrange(2, MAX_N+1):  # precompute
     for j in xrange((i+1)//2):  # dp[i] is symmetric since dp[i][j] = dp[i][i-1-j], we can only precompute left part of dp[i] to save more time
         if j > 0:
-            dp[i][j] += ((j-1)-1+1)*dp[i-1][min(j-1, (i-2)-(j-1))]   # merge any of (0,1)~(j-2,j-1), count j with ev(i-1, j-1)
-            dp[i][j] += 1.0 + dp[i-1][min(j-1, (i-2)-(j-1))]         # merge (j-1,j), count j with 1 + ev(i-1, j-1)
+            dp[i][j] += ((j-1)-1+1)*dp[i-1][min(j-1, (i-2)-(j-1))]   # merge any of (0,1)~(j-2,j-1), count j with EV(i-1, j-1)
+            dp[i][j] += 1.0 + dp[i-1][min(j-1, (i-2)-(j-1))]         # merge (j-1,j), count j with 1 + EV(i-1, j-1)
         if j < i-1:
-            dp[i][j] += 1.0 + dp[i-1][min(j, (i-2)-j)]               # merge (j,j+1), count j with 1 + ev(i-1, j)
-            dp[i][j] += ((i-1)-(j+2)+1)*dp[i-1][min(j, (i-2)-j)]     # merge any of (j+1,j+2)~(i-2,i-1), count j with ev(i-1, j)
+            dp[i][j] += 1.0 + dp[i-1][min(j, (i-2)-j)]               # merge (j,j+1), count j with 1 + EV(i-1, j)
+            dp[i][j] += ((i-1)-(j+2)+1)*dp[i-1][min(j, (i-2)-j)]     # merge any of (j+1,j+2)~(i-2,i-1), count j with EV(i-1, j)
         dp[i][j] /= (i-1)  # (i-1) choices in i cards
 
 for case in xrange(input()):
