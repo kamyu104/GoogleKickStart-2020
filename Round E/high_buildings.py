c84ffac56ce9269209f7a3d33596a4bb866d2698
Allen Liu <kamyu104@gmail.com>
2020-11-08 00:13:15 +0800

    update

---
+++ Round E/high_buildings.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2020 kamyu. All rights reserved.
+#
+# Google Kick Start 2020 Round E - Problem A. High Buildings
+# https://codingcompetitions.withgoogle.com/kickstart/round/000000000019ff47/00000000003bef73
+#
+# Time:  O(N)
+# Space: O(1)
+#
+
+def high_buildings():
+    N, A, B, C = map(int, raw_input().strip().split())
+    if A+B-C > N or (N > 1 and A == 1 and B == 1):
+        return "IMPOSSIBLE"
+    
+    result = [N-1]*(A-C) + [N]*C + [N-1]*(B-C)
+    if N-2 >= 1:
+        result = result[:1] + [N-2]*(N-(A+B-C)) + result[1:]
+    return " ".join(map(str, result))
+
+for case in xrange(input()):
+    print 'Case #%d: %s' % (case+1, high_buildings())
---
+++ Round E/longest_arithmetic.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2020 kamyu. All rights reserved.
+#
+# Google Kick Start 2020 Round E - Problem A. Longest Arithmetic
+# https://codingcompetitions.withgoogle.com/kickstart/round/000000000019ff47/00000000003bf4ed
+#
+# Time:  O(N)
+# Space: O(1)
+#
+
+def longest_arithmetic():
+    N = input()
+    A = map(int, raw_input().strip().split())
+
+    result = 0
+    d , count = None, 0
+    for i in xrange(N-1):
+        if A[i+1]-A[i] != d:
+            d = A[i+1]-A[i]
+            count = 1
+        count += 1
+        result = max(result, count)
+    return result
+
+for case in xrange(input()):
+    print 'Case #%d: %s' % (case+1, longest_arithmetic())
