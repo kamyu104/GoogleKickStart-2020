1eed99fad96ef1ba885273945851d30a3f1553cc
Allen Liu <kamyu104@gmail.com>
2020-11-08 17:20:47 +0800

    update

---
+++ Round E/golden_stone.py
@@ -0,0 +1,54 @@
+# Copyright (c) 2020 kamyu. All rights reserved.
+#
+# Google Kick Start 2020 Round E - Problem D. Golden Stone
+# https://codingcompetitions.withgoogle.com/kickstart/round/000000000019ff47/00000000003bef29
+#
+# Time:  O((N * S + M * S + R * N) * log(N * S))
+# Space: O(N * S + M * S)
+#
+
+from heapq import heappush, heappop
+
+def golden_stone():
+    N, M, S, R = map(int, raw_input().strip().split())
+    adj = [set() for _ in xrange(N)]
+    for _ in xrange(M):
+        U, V = map(int, raw_input().strip().split())
+        U -= 1
+        V -= 1
+        adj[U].add(V)
+        adj[V].add(U)
+    C = [map(lambda x: int(x)-1, raw_input().strip().split()[1:]) for _ in xrange(N)]
+    R = [map(lambda x: int(x)-1, raw_input().strip().split()[1:]) for _ in xrange(R)]
+
+    cost = [[INF for _ in xrange(S)] for _ in xrange(N)]
+    min_heap = []
+    for u in xrange(N):
+        for s in C[u]:
+            cost[u][s] = 0
+            heappush(min_heap, (0, u, s))
+    lookup = [[False for _ in xrange(S)] for _ in xrange(N)]
+    while min_heap:
+        _, u, s = heappop(min_heap)
+        if lookup[u][s]:
+            continue
+        lookup[u][s] = True
+        for v in adj[u]:  # Time: O((|V| + |E|) * log|V|) = O((N * S + M * S) * log(N * S))
+            if lookup[v][s]:
+                continue
+            if cost[u][s]+1 < cost[v][s]:
+                cost[v][s] = cost[u][s]+1
+                heappush(min_heap, (cost[v][s], v, s))
+        for r in R:  # Time: O(R * N * log|V|) = O(R * N * log(N * S))
+            if lookup[u][r[-1]]:
+                continue
+            total = sum(cost[u][r[i]] for i in xrange(len(r)-1))  # Time: O(K) = O(3)
+            if total < cost[u][r[-1]]:
+                cost[u][r[-1]] = total
+                heappush(min_heap, (cost[u][r[-1]], u, r[-1]))
+    result = min(cost[u][0] for u in xrange(N))
+    return result if result < INF else -1
+
+INF = 10**12
+for case in xrange(input()):
+    print 'Case #%d: %s' % (case+1, golden_stone())
