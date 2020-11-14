65acb3f522931e5e8be2769aa5e1fb70ddab8b33
Allen Liu <kamyu104@gmail.com>
2020-11-14 20:16:41 +0800

    update

--- Round D/beauty_of_tree.py
+++ Round D/beauty_of_tree.py
@@ -11,26 +11,26 @@ from itertools import izip
 from functools import partial
 
 def iter_dfs(adj, A, B):
-    def divide(A, B, curr, d, stk, A_prefix, B_prefix, A_cnt, B_cnt):
+    def divide(curr, d):
         prev_A_cnt, prev_B_cnt = [0], [0]
-        stk.append(partial(postprocess, A, B, curr, d, A_prefix, B_prefix, prev_A_cnt, prev_B_cnt, A_cnt, B_cnt))
+        stk.append(partial(postprocess, curr, d, prev_A_cnt, prev_B_cnt))
         for node in reversed(adj[curr]):
-            stk.append(partial(divide, A, B, node, d+1, stk, A_prefix, B_prefix, A_cnt, B_cnt))
-        stk.append(partial(preprocess, A, B, d, prev_A_cnt, prev_B_cnt, A_prefix, B_prefix))
+            stk.append(partial(divide, node, d+1))
+        stk.append(partial(preprocess, d, prev_A_cnt, prev_B_cnt))
 
-    def preprocess(A, B, d, prev_A_cnt, prev_B_cnt, A_prefix, B_prefix):
+    def preprocess(d, prev_A_cnt, prev_B_cnt):
         prev_A_cnt[0] = A_prefix[d%A]
         prev_B_cnt[0] = B_prefix[d%B]
         A_prefix[d%A] += 1
         B_prefix[d%B] += 1
 
-    def postprocess(A, B, curr, d, A_prefix, B_prefix, prev_A_cnt, prev_B_cnt, A_cnt, B_cnt):
+    def postprocess(curr, d, prev_A_cnt, prev_B_cnt):
         A_cnt[curr] = A_prefix[d%A]-prev_A_cnt[0]
         B_cnt[curr] = B_prefix[d%B]-prev_B_cnt[0]
 
     A_prefix, B_prefix, A_cnt, B_cnt = [0]*len(adj), [0]*len(adj), [0]*len(adj), [0]*len(adj)
     stk = []
-    stk.append(partial(divide, A, B, 0, 0, stk, A_prefix, B_prefix, A_cnt, B_cnt))
+    stk.append(partial(divide, 0, 0))
     while stk:
         stk.pop()()
     return A_cnt, B_cnt
