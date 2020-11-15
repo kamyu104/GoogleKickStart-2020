932e9d55d8f3db143b50292a99b9082bbd334dd7
Allen Liu <kamyu104@gmail.com>
2020-11-15 20:18:02 +0800

    update

--- Round H/friends.py
+++ Round H/friends.py
@@ -4,7 +4,7 @@
 # https://codingcompetitions.withgoogle.com/kickstart/round/000000000019ff49/000000000043b027
 #
 # Time:  O(A^3 + A^2 * (N + Q)) = O(N + Q) since O(A) = O(26), pass in PyPy2 but Python2
-# Space: O(A^2 + N)
+# Space: O(N + A^2)
 #
 
 def floyd_warshall(dist):
