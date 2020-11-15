c0abcfb83eba34a9085691536b606966859a1cfd
Allen Liu <kamyu104@gmail.com>
2020-11-15 18:16:16 +0800

    update

---
+++ Round H/rugby.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2020 kamyu. All rights reserved.
+#
+# Google Kick Start 2020 Round H - Problem C. Rugby
+# https://codingcompetitions.withgoogle.com/kickstart/round/000000000019ff49/000000000043b027
+#
+# Time:  O(NlogN)
+# Space: O(N)
+#
+
+def rugby():
+    N = input()
+    X, Y = [0]*N, [0]*N
+    for i in xrange(N):
+        X[i], Y[i] = map(int, raw_input().strip().split())
+
+    Y.sort()
+    result = sum(abs(y-Y[(N-1)//2]) for y in Y)
+    X.sort()
+    X = [x-i for i, x in enumerate(X)]
+    X.sort()
+    result += sum(abs(x-X[(N-1)//2]) for x in X)
+    return result
+
+for case in xrange(input()):
+    print 'Case #%d: %s' % (case+1, rugby())
